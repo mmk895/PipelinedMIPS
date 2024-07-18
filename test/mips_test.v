2c9b99aaf6d9b87e889a41a5f31f31a2389fdb3d
mmk895 <kalanimohammad895@gmail.com>
2024-07-18 23:44:58 +0330

    Update mips_test.v

--- test/mips_test.v
+++ test/mips_test.v
@@ -7,24 +7,24 @@ module mips_test;
         .reset(reset)
     );
 
-    integer i;
-    
     initial begin
         $dumpfile("mips_test.vcd");
-        $dumpvars(0,mips_test);
+        $dumpvars(0, clk, reset, Mips);
 
-        for (i = 0; i < 8; i = i + 1)
-        $dumpvars(0,mips_test.Mips.Decode_Stage.Register_File.r_registers[i]);
+        // Add registers to the waveform
+        for (integer i = 0; i < 8; i = i + 1)
+            $dumpvars(0, Mips.Decode_Stage.Register_File.r_registers[i]);
 
-        for (i = 0; i < 1024; i = i + 1)
-        $dumpvars(0,mips_test.Mips.Memory_Stage.Data_Memory.memory[i]);
+        // Add memory to the waveform
+        for (integer i = 0; i < 1024; i = i + 1)
+            $dumpvars(0, Mips.Memory_Stage.Data_Memory.memory[i]);
 
         clk = 0;
         reset = 1;
         #10
         clk = 1;
         reset = 0;
-        for (i = 0; i < 500; i = i + 1)
+        for (integer i = 0; i < 500; i = i + 1)
             #10 clk = ~clk;    
     end
            
